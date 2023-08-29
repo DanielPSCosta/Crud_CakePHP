6e470625a41581d51eb1ca2aad7474dc1909d4aa
Daniel Pereira Dos Santos Costa <57815094+DanielPSCosta@users.noreply.github.com>
2023-08-29 17:22:28 -0300

    Update routes.php

--- cakephp-2.10.24/app/Config/routes.php
+++ cakephp-2.10.24/app/Config/routes.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Routes configuration
  *
@@ -19,26 +20,27 @@
  * @since         CakePHP(tm) v 0.2.9
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
- 
+
 /**
  * Here, we are connecting '/' (base path) to controller called 'Pages',
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+/* Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));*/
+Router::connect('/', array('controller' => 'Filmes', 'action' => 'index'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
  */
-	CakePlugin::routes();
+CakePlugin::routes();
 
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
  * the built-in default routes.
  */
-	require CAKE . 'Config' . DS . 'routes.php';
+require CAKE . 'Config' . DS . 'routes.php';
